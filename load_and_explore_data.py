16594ba32439edcee4a40723a08c6fa22c993be6
Bill Psomas <psomasbill@gmail.com>
2019-06-08 00:57:52 +0300

    Update load_and_explore_data.py

--- load_and_explore_data.py
+++ load_and_explore_data.py
@@ -34,107 +34,107 @@ image_size = image_shape[0]
 sign_classes, class_indices, class_counts = np.unique(y_train, return_index = True, return_counts = True)
 n_classes = class_counts.shape[0]
 
-##Print quick data summary
-#print("Number of training examples =", n_train)
-#print("Number of testing examples =", n_test)
-#print("Number of validation examples: ", n_valid)
-#print("Image data shape =", image_shape)
-#print("Number of classes =", n_classes)
-#
-##Define the function that plots the number of instances per class
-#def plot_instance_counts(dataset, name="dataset"):
-#    from collections import Counter
-#    counts = Counter(dataset)
-#    labels, values = zip(*counts.items())
-#    indexes = np.arange(len(labels))
-#    width = 0.5
-#    with plt.style.context(('seaborn-muted')):
-#        figure = plt.figure(figsize=(12, 3))
-#        plt.bar(indexes, values, width)
-#        plt.xticks(indexes + width * 0.5, labels)
-#        plt.xlabel('Class Label')
-#        plt.title('{} : Number of instance per class'.format(name))
-#        plt.show()
-#
-#plot_instance_counts(y_train, 'Training Data')
-#
-##Define the function that returns the indices of images that belong to a given class
-#def indices_for_class(class_label, labels=y_train):
-#    return np.where(labels == [class_label])[0]
-#
-## Plot classes
-#try:
-#    os.mkdir('./figs')
-#except:
-#    print('Folder exists')
-#with open(train_dataset_file, mode='rb') as f:
-#    images = pickle.load(f)['features']
-#    n = 5
-#    img = list([])
-#    plt.rcParams["figure.figsize"] = [8,2]
-#    fig, axs = plt.subplots(1, 5, constrained_layout=True)
-#
-#    for cl in range(n_classes):
-#        indices = np.random.choice(indices_for_class(cl), n)
-#        for i, index in enumerate(indices):
-#            axs[i].imshow(images[index], interpolation='nearest', cmap='jet')
-#            axs[i].axis('off')
-#        plt.suptitle('Images for class: {}'.format(signnames[cl]))
-#        plt.savefig('./figs/class'+str(cl)+'.png')
-#    plt.close()
-#
-#image_list = []
-#url = './figs/*.png'
-#for filename in glob.glob(url):
-#    image_list.append(Image.open(filename))
-#
-#w, h = image_list[0].size
-#
-#for j in range(8):
-#    offset = 0
-#    new_im = Image.new('RGB', (w, h*5))
-#    for i in range(5):
-#      new_im.paste(image_list[i + j*5], (0,offset))
-#      offset += image_list[i].size[1]
-#    new_im.save('./classes'+str(i + j*5 - 4)+'-'+str(i + j*5)+'.png')
-#offset = 0
-#new_im = Image.new('RGB', (w, h*3))
-#for i in range(3):
-#  new_im.paste(image_list[40 + i], (0,offset))
-#  offset += image_list[i].size[1]
-#new_im.save('./classes40-42.png')
-#
-##for i in range(43):
-##    os.remove('./class'+str(i)+'.png')
-#
-##Define the function that plots 5 sample images for each classs
-#def plot_n_images_for_class(class_label, n=5, dataset_source=train_dataset_file, labels=y_train, cmap='jet'):
-#    with open(dataset_source, mode='rb') as f:
-#        images = pickle.load(f)['features']
-#        indices = np.random.choice(indices_for_class(class_label), n)
-#        figure = plt.figure(figsize = (6,6))
-#
-#        for i, index in enumerate(indices):
-#            a = figure.add_subplot(n,n, i+1)
-#            img = plt.imshow(images[index], interpolation='nearest', cmap=cmap)
-#            plt.axis('off')
-#        plt.suptitle('images for class {}'.format(class_label))
-#        plt.show()
-#
-#for i in np.arange(43):
-#    plot_n_images_for_class(i)
-#
-##Define the function that plots the number of instances per class for a given subset
-#def histogram_plot(dataset, label):
-#    hist, bins = np.histogram(dataset, bins=n_classes)
-#    width = 0.7 * (bins[1] - bins[0])
-#    center = (bins[:-1] + bins[1:]) / 2
-#    plt.bar(center, hist, align='center', width=width)
-#    plt.xlabel(label)
-#    plt.ylabel("Image count")
-#    plt.show()
-#    
-##Plotting instance counts for each subset
-#histogram_plot(y_train, "Training examples")
-#histogram_plot(y_test, "Testing examples")
-#histogram_plot(y_valid, "Validation examples")
+#Print quick data summary
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Number of validation examples: ", n_valid)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+#Define the function that plots the number of instances per class
+def plot_instance_counts(dataset, name="dataset"):
+    from collections import Counter
+    counts = Counter(dataset)
+    labels, values = zip(*counts.items())
+    indexes = np.arange(len(labels))
+    width = 0.5
+    with plt.style.context(('seaborn-muted')):
+        figure = plt.figure(figsize=(12, 3))
+        plt.bar(indexes, values, width)
+        plt.xticks(indexes + width * 0.5, labels)
+        plt.xlabel('Class Label')
+        plt.title('{} : Number of instance per class'.format(name))
+        plt.show()
+
+plot_instance_counts(y_train, 'Training Data')
+
+#Define the function that returns the indices of images that belong to a given class
+def indices_for_class(class_label, labels=y_train):
+    return np.where(labels == [class_label])[0]
+
+# Plot classes
+try:
+    os.mkdir('./figs')
+except:
+    print('Folder exists')
+with open(train_dataset_file, mode='rb') as f:
+    images = pickle.load(f)['features']
+    n = 5
+    img = list([])
+    plt.rcParams["figure.figsize"] = [8,2]
+    fig, axs = plt.subplots(1, 5, constrained_layout=True)
+
+    for cl in range(n_classes):
+        indices = np.random.choice(indices_for_class(cl), n)
+        for i, index in enumerate(indices):
+            axs[i].imshow(images[index], interpolation='nearest', cmap='jet')
+            axs[i].axis('off')
+        plt.suptitle('Images for class: {}'.format(signnames[cl]))
+        plt.savefig('./figs/class'+str(cl)+'.png')
+    plt.close()
+
+image_list = []
+url = './figs/*.png'
+for filename in glob.glob(url):
+    image_list.append(Image.open(filename))
+
+w, h = image_list[0].size
+
+for j in range(8):
+    offset = 0
+    new_im = Image.new('RGB', (w, h*5))
+    for i in range(5):
+        new_im.paste(image_list[i + j*5], (0,offset))
+        offset += image_list[i].size[1]
+    new_im.save('./classes'+str(i + j*5 - 4)+'-'+str(i + j*5)+'.png')
+offset = 0
+new_im = Image.new('RGB', (w, h*3))
+for i in range(3):
+    new_im.paste(image_list[40 + i], (0,offset))
+    offset += image_list[i].size[1]
+new_im.save('./classes40-42.png')
+
+#for i in range(43):
+#    os.remove('./class'+str(i)+'.png')
+
+#Define the function that plots 5 sample images for each classs
+def plot_n_images_for_class(class_label, n=5, dataset_source=train_dataset_file, labels=y_train, cmap='jet'):
+    with open(dataset_source, mode='rb') as f:
+        images = pickle.load(f)['features']
+        indices = np.random.choice(indices_for_class(class_label), n)
+        figure = plt.figure(figsize = (6,6))
+
+        for i, index in enumerate(indices):
+            a = figure.add_subplot(n,n, i+1)
+            img = plt.imshow(images[index], interpolation='nearest', cmap=cmap)
+            plt.axis('off')
+        plt.suptitle('images for class {}'.format(class_label))
+        plt.show()
+
+for i in np.arange(43):
+    plot_n_images_for_class(i)
+
+#Define the function that plots the number of instances per class for a given subset
+def histogram_plot(dataset, label):
+    hist, bins = np.histogram(dataset, bins=n_classes)
+    width = 0.7 * (bins[1] - bins[0])
+    center = (bins[:-1] + bins[1:]) / 2
+    plt.bar(center, hist, align='center', width=width)
+    plt.xlabel(label)
+    plt.ylabel("Image count")
+    plt.show()
+    
+#Plotting instance counts for each subset
+histogram_plot(y_train, "Training examples")
+histogram_plot(y_test, "Testing examples")
+histogram_plot(y_valid, "Validation examples")
